9a585d590e2e4442def4ea1e8c9d3fddc0cf52cc
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-17 23:56:25 -0300

    iniciando init

---
+++ index.js
@@ -0,0 +1,27 @@
+// const http = require("http");
+
+// const hostname = "127.0.0.1";
+// const port = 5000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+
+//   res.end("Hello JM!");
+// });
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+
+//   res.end("Hello JM!");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log("deu bom");
+// });
+
+
+
+
+const git status
